e866d14b2679cb7822a3de6ca34a03cfd2cd70e8
Fred Lintz <fredlintz@Freds-MacBook-Pro.local>
2017-08-08 00:40:08 -0600

    bootstrapped whole thing, got rid of annoying alerts, changed background, etc

--- assets/javascript/newJS.js
+++ assets/javascript/newJS.js
@@ -10,10 +10,6 @@
 	var compScore = 0;
 
 
-setTimeout(function () {
-	    alert("Try to beat the Machine, best out of 3 determines the fate of the world. Ties will not be recorded.");
-	}, 700);
-
 gamePlay("#paper", paper, 1, 2);
 gamePlay("#rock", rock, 0, 1);
 gamePlay("#scissor", scissor, 2, 0);
@@ -22,28 +18,20 @@ gamePlay("#scissor", scissor, 2, 0);
 function gamePlay(divId, image, num1, num2) { 
 	$(divId).click(function() {
 
-		//create random nuber to choose random computer image, display that image
-		//in corresponding html ID
 	    var randomNumber = Math.floor((Math.random() * compArray.length));
 		$("#player").html(image);
 		$("#computer").html(compArray[randomNumber]);
 
-			//else if logic to determine who won and lost each hand
 			if (randomNumber === num1) {
 				console.log("Tie")
-			}
-
-			else if (randomNumber === num2) {
+			} else if (randomNumber === num2) {
 				compScore++
 				$("#compScore").html(compScore);
-			}
-
-			else {
+			} else {
 				humanScore++
 				$("#humanScore").html(humanScore);
 			}
 		
-		//delay checkScore so images can load, and score can update before alerting outcome	
 		setTimeout(function () {
 			checkScores();			
 		}, 500);
@@ -56,10 +44,21 @@ function gamePlay(divId, image, num1, num2) {
 function checkScores() {
 
 	if (compScore === 3) {
-		alert("You Lost! May God have mercy on our souls... Refresh Page for New Game")
+		$("#battleground").css("display", "none");
+		$("#hidden div").append("<h1>You LOST, Loser</h1>");
+		$("#hidden").css("display", "inherit");
+		setTimeout (function() {
+			location.reload();	
+		}, 3000);		
+	
 
 	} else if (humanScore === 3) {
-		alert("You WON! Thank you Chuck Norris! Refresh Page for New Game")
+		$("#battleground").css("display", "none");
+		$("#hidden div").append("<h1>You WON, Wahoo!</h1>");
+		$("#hidden").css("display", "inherit");
+		setTimeout (function() {
+			location.reload();	
+		}, 3000);			
 	}
 };
 
